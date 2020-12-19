6152117d54e554bb7cf126ae437226b99c1102de
Huynh Huy Hiep <huynhhuyhiep0808@gmail.com>
2020-12-19 22:13:37 +0700

    Fix eslint

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import ReactDom from 'react-dom';
 import { cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
-
+import React from 'react';
 import Button from './Button';
 
 afterEach(cleanup);
