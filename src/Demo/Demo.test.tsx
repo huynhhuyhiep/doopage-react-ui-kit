c044162a8a79a812295ae4bd768379ea155664bb
Huynh Huy Hiep <huynhhuyhiep0808@gmail.com>
2020-12-19 16:32:24 +0700

    Remove snapshot test

--- src/Demo/Demo.test.tsx
+++ src/Demo/Demo.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { cleanup, render } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 
 import '@testing-library/jest-dom/extend-expect';
 
-import Demo, { DemoProps } from '../Demo';
+import Demo, { DemoProps } from './Demo';
 
 afterEach(cleanup);
 
@@ -21,9 +20,4 @@ it('render Demo Component correctly', () => {
 	};
 	const { getByTestId } = render(<Demo {...props}>Hello World</Demo>);
 	expect(getByTestId('DemoMessage')).toHaveTextContent('Hello World');
-});
-
-it('matches snapshot', () => {
-	const tree = renderer.create(<Demo type={'danger'}>Errors</Demo>).toJSON();
-	expect(tree).toMatchSnapshot();
 });
